40c4cb5b307ab2cc10cceb304905ddf1fc4fc50c
Cristian Abrante <cristian@abrante.me>
2020-01-03 20:33:03 +0000

    Add: project structure setup in linux and macOS

---
+++ src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
@@ -0,0 +1,10 @@
+package master2019.flink.YellowTaxiTrip;
+
+/**
+ * In this class the JFK airport trips program has to be implemented.
+ */
+public class JFKAlarms {
+    public static void main(String[] args){
+        System.out.println("JFK Alarms prediction");
+    }
+}
---
+++ src/main/java/master2019/flink/YellowTaxiTrip/LargeTrips.java
@@ -0,0 +1,10 @@
+package master2019.flink.YellowTaxiTrip;
+
+/**
+ * In this class the Large trips program has to be implemented
+ */
+public class LargeTrips {
+    public static void main(String[] args){
+        System.out.println("Large trips prediction");
+    }
+}
