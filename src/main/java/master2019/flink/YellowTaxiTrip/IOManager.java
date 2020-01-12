76b1a4aec2c27dff93d9f9097b1a75af7eb0420d
Cristian Abrante <cristian@abrante.me>
2020-01-12 23:20:04 +0100

    JFK finished

--- src/main/java/master2019/flink/YellowTaxiTrip/IOManager.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/IOManager.java
@@ -3,7 +3,11 @@ package master2019.flink.YellowTaxiTrip;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple18;
+import org.apache.flink.api.java.tuple.Tuple9;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class IOManager {
   // String specifing option.
@@ -18,33 +22,12 @@ public class IOManager {
    * @return the dataframe
    * @throws IllegalArgumentException if option not specified.
    */
-  public static DataSet<YellowTaxyData> generateDataSetFromParams(
-      ExecutionEnvironment env, ParameterTool params) throws IllegalArgumentException {
-    DataSet<YellowTaxyData> data;
+  public static DataStream<YellowTaxyData> generateDataSetFromParams(
+      StreamExecutionEnvironment env, ParameterTool params) throws IllegalArgumentException {
+    DataStream<YellowTaxyData> data;
     if (params.has(IOManager.INPUT_OPTION)) {
       data =
-          env.readCsvFile(params.getRequired(IOManager.INPUT_OPTION))
-              .pojoType(
-                  YellowTaxyData.class,
-                  "vendorid",
-                  "tpeppickupdatetime",
-                  "tpepdropoffdatetime",
-                  "passengercount",
-                  "tripdistance",
-                  "ratecodeid",
-                  "storeandfwdflag",
-                  "pulocationid",
-                  "dolocationid",
-                  "paymenttype",
-                  "fareamount",
-                  "extra",
-                  "mtatax",
-                  "tipamount",
-                  "tollsamount",
-                  "improvementsurcharge",
-                  "totalamount",
-                  "congestionamount")
-              .map(new YellowTaxiCreator());
+          env.readTextFile(params.getRequired(IOManager.INPUT_OPTION)).map(new YellowTaxiCreator());
       return data;
     } else {
       throw new IllegalArgumentException(
@@ -52,29 +35,139 @@ public class IOManager {
     }
   }
 
-  public static final class YellowTaxiCreator
-      implements MapFunction<YellowTaxyData, YellowTaxyData> {
+  public static DataSet<
+          Tuple9<Integer, String, String, Integer, Integer, String, String, String, String>>
+      getTestData(ExecutionEnvironment env, ParameterTool params) {
+    DataSet<Tuple9<Integer, String, String, Integer, Integer, String, String, String, String>>
+        data =
+            env.readCsvFile(params.getRequired("input"))
+                .types(
+                    Integer.class,
+                    String.class,
+                    String.class,
+                    Integer.class,
+                    Integer.class,
+                    Integer.class,
+                    String.class,
+                    Double.class,
+                    Double.class,
+                    Double.class,
+                    Double.class,
+                    Double.class,
+                    Double.class,
+                    Double.class,
+                    Double.class,
+                    Double.class,
+                    Double.class,
+                    Double.class)
+                .map(
+                    new MapFunction<
+                        Tuple18<
+                            Integer,
+                            String,
+                            String,
+                            Integer,
+                            Integer,
+                            Integer,
+                            String,
+                            Double,
+                            Double,
+                            Double,
+                            Double,
+                            Double,
+                            Double,
+                            Double,
+                            Double,
+                            Double,
+                            Double,
+                            Double>,
+                        Tuple9<
+                            Integer,
+                            String,
+                            String,
+                            Integer,
+                            Integer,
+                            String,
+                            String,
+                            String,
+                            String>>() {
+                      @Override
+                      public Tuple9<
+                              Integer,
+                              String,
+                              String,
+                              Integer,
+                              Integer,
+                              String,
+                              String,
+                              String,
+                              String>
+                          map(
+                              Tuple18<
+                                      Integer,
+                                      String,
+                                      String,
+                                      Integer,
+                                      Integer,
+                                      Integer,
+                                      String,
+                                      Double,
+                                      Double,
+                                      Double,
+                                      Double,
+                                      Double,
+                                      Double,
+                                      Double,
+                                      Double,
+                                      Double,
+                                      Double,
+                                      Double>
+                                  data)
+                              throws Exception {
+                        String[] timestampSplit = data.f1.split(" ");
+                        String datePart = timestampSplit[0];
+                        String timePart = timestampSplit[1];
+
+                        String[] datePartSplit = datePart.split("-");
+                        String[] timePartSplit = timePart.split(":");
+                        return new Tuple9<>(
+                            data.f0,
+                            data.f1,
+                            data.f2,
+                            data.f3,
+                            data.f5,
+                            datePartSplit[0],
+                            datePartSplit[1],
+                            datePartSplit[2],
+                            timePartSplit[0]);
+                      }
+                    });
+    return data;
+  }
+
+  public static final class YellowTaxiCreator implements MapFunction<String, YellowTaxyData> {
     @Override
-    public YellowTaxyData map(YellowTaxyData yellowTaxyData) throws Exception {
+    public YellowTaxyData map(String line) throws Exception {
+      String[] splittedLine = line.split(",");
       return new YellowTaxyData(
-          yellowTaxyData.getVendorid(),
-          yellowTaxyData.getTpeppickupdatetime(),
-          yellowTaxyData.getTpepdropoffdatetime(),
-          yellowTaxyData.getPassengercount(),
-          yellowTaxyData.getTripdistance(),
-          yellowTaxyData.getRatecodeid(),
-          yellowTaxyData.getStoreandfwdflag(),
-          yellowTaxyData.getPulocationid(),
-          yellowTaxyData.getDolocationid(),
-          yellowTaxyData.getPaymenttype(),
-          yellowTaxyData.getFareamount(),
-          yellowTaxyData.getExtra(),
-          yellowTaxyData.getMtatax(),
-          yellowTaxyData.getTipamount(),
-          yellowTaxyData.getTollsamount(),
-          yellowTaxyData.getImprovementsurcharge(),
-          yellowTaxyData.getTotalamount(),
-          yellowTaxyData.getCongestionamount());
+          Integer.parseInt(splittedLine[0]),
+          splittedLine[1],
+          splittedLine[2],
+          Integer.parseInt(splittedLine[3]),
+          Double.parseDouble(splittedLine[4]),
+          Integer.parseInt(splittedLine[5]),
+          splittedLine[6],
+          Integer.parseInt(splittedLine[7]),
+          Integer.parseInt(splittedLine[8]),
+          Integer.parseInt(splittedLine[9]),
+          Double.parseDouble(splittedLine[10]),
+          Double.parseDouble(splittedLine[11]),
+          Double.parseDouble(splittedLine[12]),
+          Double.parseDouble(splittedLine[13]),
+          Double.parseDouble(splittedLine[14]),
+          Double.parseDouble(splittedLine[15]),
+          Double.parseDouble(splittedLine[16]),
+          Double.parseDouble(splittedLine[17]));
     }
   }
 }
--- src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
@@ -1,18 +1,28 @@
 package master2019.flink.YellowTaxiTrip;
 
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.GroupReduceFunction;
-import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.*;
+import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.api.java.tuple.*;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.IterableIterator;
 import scala.Int;
 
-import java.nio.file.FileSystem;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 
 /** In this class the JFK airport trips program has to be implemented. */
@@ -23,18 +33,37 @@ public class JFKAlarms {
 
     // Load the execution environment. We do not need StreamExecutionEnvironment
     // as we are using static files.
-    ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
     env.setParallelism(1);
 
     try {
-      DataSet<Tuple4<Integer, String, String, Integer>> counts =
+
+      DataStream<Tuple4<Integer, String, String, Integer>> counts =
           IOManager.generateDataSetFromParams(env, params)
               .filter(new DataSetFilter())
-              .groupBy("vendorid", "pickupyear", "pickupmonth", "pickupday", "pickuphour")
-              .reduceGroup(new JFKGroupReducer());
+              .assignTimestampsAndWatermarks(
+                  new AscendingTimestampExtractor<YellowTaxyData>() {
+                    @Override
+                    public long extractAscendingTimestamp(YellowTaxyData yellowTaxyData) {
+                      String pattern = "yyyy-MM-dd HH:mm:ss"; // ex: 2019-06-01 00:55:13
+                      SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+                      Date datePU = null;
+                      try {
+                        datePU = simpleDateFormat.parse(yellowTaxyData.getTpeppickupdatetime());
+                      } catch (ParseException e) {
+                        e.printStackTrace();
+                      }
+                      return datePU.getTime();
+                    }
+                  })
+              .keyBy("vendorid")
+              .window(TumblingEventTimeWindows.of(Time.hours(1)))
+              .apply(new JFKApply());
+
       if (params.has(IOManager.OUTPUT_OPTION)) {
-        counts.writeAsCsv(params.getRequired(IOManager.OUTPUT_OPTION));
+        counts.writeAsCsv(
+            params.getRequired(IOManager.OUTPUT_OPTION), FileSystem.WriteMode.OVERWRITE);
         env.execute();
       } else {
         counts.print();
@@ -45,27 +74,17 @@ public class JFKAlarms {
     }
   }
 
-  public static final class DataSetFilter implements FilterFunction<YellowTaxyData> {
-    public static int JFK_CODE = 2;
-    public static int MIN_NUMBER_OF_PASSENGERS = 2;
-
-    @Override
-    public boolean filter(YellowTaxyData data) {
-      return data.getRatecodeid() == DataSetFilter.JFK_CODE
-          && data.getPassengercount() >= DataSetFilter.MIN_NUMBER_OF_PASSENGERS;
-    }
-  }
-
-  public static final class JFKGroupReducer
-      implements GroupReduceFunction<YellowTaxyData, Tuple4<Integer, String, String, Integer>> {
-
+  public static final class JFKApply
+      implements WindowFunction<
+          YellowTaxyData, Tuple4<Integer, String, String, Integer>, Tuple, TimeWindow> {
     public static Integer DEFAULT_VENDOR_ID = -1;
     public static String DEFAULT_PICKUP_DATE = "";
     public static String DEFAULT_DROPOFF_DATE = "";
     public static Integer DEFAULT_PASSENGERS_COUNT = 0;
-
     @Override
-    public void reduce(
+    public void apply(
+        Tuple tuple,
+        TimeWindow timeWindow,
         Iterable<YellowTaxyData> iterable,
         Collector<Tuple4<Integer, String, String, Integer>> collector)
         throws Exception {
@@ -94,7 +113,19 @@ public class JFKAlarms {
         passengersCount += data.getPassengercount();
       }
 
-      collector.collect(new Tuple4<>(vendorId, pickupDate, dropoffDate, passengersCount));
+      collector.collect(
+              new Tuple4<>(vendorId, pickupDate, dropoffDate, passengersCount));
+    }
+  }
+
+  public static final class DataSetFilter implements FilterFunction<YellowTaxyData> {
+    public static int JFK_CODE = 2;
+    public static int MIN_NUMBER_OF_PASSENGERS = 2;
+
+    @Override
+    public boolean filter(YellowTaxyData data) {
+      return data.getRatecodeid() == DataSetFilter.JFK_CODE
+          && data.getPassengercount() >= DataSetFilter.MIN_NUMBER_OF_PASSENGERS;
     }
   }
 }
